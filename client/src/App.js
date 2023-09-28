a0c4573d8a53f3b5ae931d05b5156ca11cc46c0a
Shahvez Ahmad <shahvez.ahmad2002@gmail.com>
2023-09-28 21:14:51 +0530

    Initialised - React + Tailwind

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-gray-200 p-4">
+      <h1 className="text-3xl font-bold text-blue-700">Hello, Tailwind CSS!</h1>
+      <p className="text-gray-600 mt-2">This is a React app with Tailwind CSS.</p>
+    </div>
+  );
+}
+
+export default App;
