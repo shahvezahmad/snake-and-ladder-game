3cc1a58b5b1dfb13ef40d75988e2001b0919480a
Shahvez Ahmad <shahvez.ahmad2002@gmail.com>
2023-10-02 15:34:31 +0530

    login error fix

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -5,17 +5,19 @@ const login = async (req, res) => {
     try {
         const { email, password } = req.body
         const user = await User.findOne({ email })
-        const isPasswordCorrect = await user.comparePassword(password)
+        const isPasswordCorrect = await user.comparePassword(password);
         if (isPasswordCorrect) {
             const tokens = await User.findOne({ email })
             if(user.verify===false){
                 return res.status(401).json({ msg:"PLEASE VERIFY YOUR EMAIL FIRST" })
             }
             else{
-                return res.status(201).json({msg:"Login Sucessful"});
+                // console.log("here");
+                return res.status(201).json({msg:"OK"});
             }
         }
         else {
+            // console.log("here");
             res.status(StatusCodes.OK).json({ err: "Credentials do not match" })
         }
     } catch (error) {
